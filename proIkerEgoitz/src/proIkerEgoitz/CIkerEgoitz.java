4eebc7fda4c363bdd2c7ba65045a6f84fca65e6b
INTEL <INTEL@DESKTOP-8DHARR8>
2021-04-09 13:59:57 +0200

    cambio

--- proIkerEgoitz/src/proIkerEgoitz/CIkerEgoitz.java
+++ proIkerEgoitz/src/proIkerEgoitz/CIkerEgoitz.java
@@ -4,7 +4,7 @@ public class CIkerEgoitz {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo Primos y Primas y Abuelos y Abuelas y Tios e Hijos y enemigos");
+		System.out.println("Hola Mundo Primos y Primas y Abuelos y Abuelas y Tios e Hijos y Amigos");
 
 	}
 
