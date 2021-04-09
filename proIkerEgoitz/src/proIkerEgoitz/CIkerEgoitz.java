39cf68647b8ed5eea9a2a09d50e1658e0e6275ad
Beatriz <Beatriz@DESKTOP-SP31TMV>
2021-04-09 13:31:11 +0200

    PruebaEgoitz

---
+++ proIkerEgoitz/src/proIkerEgoitz/CIkerEgoitz.java
@@ -0,0 +1,11 @@
+package proIkerEgoitz;
+
+public class CIkerEgoitz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo Primos y Primas y Abuelos y Abuelas y Tios");
+
+	}
+
+}
